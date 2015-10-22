0fc75ca1cb7e65a71f7dd54366f5c7733b6f8728
Paulo Cunha <paulomrcunha@gmail.com>
2015-10-23 00:18:05 +0200

    feature: Enabled Google Maps

--- src/js/main.js
+++ src/js/main.js
@@ -7,4 +7,15 @@ function AppViewModel() {
 }
 
 //Activates knockout.js
-ko.applyBindings(new AppViewModel());
+//ko.applyBindings(new AppViewModel());
+
+
+//Testing Google Maps API
+
+var map;
+function initMap() {
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: -34.397, lng: 150.644},
+		zoom: 8
+	});
+}
